e06c0223e036f9f94c71c1387e080d15185cd838
poronite <72969946+poronite@users.noreply.github.com>
2021-03-16 12:08:26 +0000

    Preload Scene
    Creation of Preload Scene.

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+    public static Manager ManagerInstance = null;
+
+    private void Awake()
+    {
+        ManagerInstance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+}
