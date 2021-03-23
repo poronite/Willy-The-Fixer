738292c22559b6744e3659f198d7554bfd43f734
poronite <72969946+poronite@users.noreply.github.com>
2021-03-23 12:50:29 +0000

    Sprint
    Sprint of the player.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,13 +7,25 @@ public class Player : MonoBehaviour
 {
     PlayerInput input;
     Vector2 move;
-    public float speed = 0;
+    public float Speed = 0;
+    public float SpeedMultiplier = 0;
 
     private void Awake()
     {
+        SetInputs();
+    }
+
+    private void SetInputs()
+    {
         input = new PlayerInput();
+
+        //Movement
         input.Player.Move.performed += context => move = context.ReadValue<Vector2>();
         input.Player.Move.canceled += context => move = Vector2.zero;
+
+        //Sprint
+        input.Player.Sprint.performed += context => Speed *= SpeedMultiplier;
+        input.Player.Sprint.canceled += context => Speed /= SpeedMultiplier;
     }
 
     private void OnEnable()
@@ -28,7 +40,7 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 movement = new Vector3(move.x, 0.0f, move.y) * speed * Time.deltaTime;
+        Vector3 movement = new Vector3(move.x, 0.0f, move.y) * Speed * Time.deltaTime;
         transform.Translate(movement, Space.World);
     }
 }
